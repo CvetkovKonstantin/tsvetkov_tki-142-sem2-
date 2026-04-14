a5f8a3dafa7f14039cab3b1faad27fa608f73ace
CvetkovKonstantin <praimrprrt@gmail.com>
2026-04-14 16:29:42 +0300

    Class_Rectangle.h

--- Class_Rectangle.h
+++ Class_Rectangle.h
@@ -1,54 +1,22 @@
 #pragma once
 #include "Class_Point.h"
 
-/**
-* @brief Класс прямоугольника по трем точкам
-*/
 class Rectangle
 {
 private:
-	/**
-	* @param p1, p2, p3 - точки прямоугольника
-	*/
 	Point p1, p2, p3;
 
-	/**
-	* @brief Константа для безопасного сравнения вещественных чисел 
-	*/
-	const double EPS = 1e-7;
+	double side1, side2, diagonal;
 
-	/**
-	* @brief Вспомогательная функция для получения квадратов сторон 
-	* @param s - массив для записи результатов
-	*/
-	void GetSideSquares(double s[3]) const;
+	static const double EPS;
 
-	/**
-	* @brief Функция проверки возможности существования прямоугольника
-	*/
-	void CheckRectangle() const;
+
+	void Init();
 
 public:
-
-	/**
-	* @brief Конструктор по-умолчанию
-	*/
 	Rectangle();
+	Rectangle(Point t1, Point t2, Point t3);
 
-	/**
-	* @brief Конструктор с параметрами 
-	*/
-	Rectangle(const Point p1, const Point p2, const Point p3);
-
-	/**
-	* @brief Рассчитывает площадь прямоугольника
-	* @return Площадь прямоугольника
-	*/
 	double Area() const;
-
-	/**
-	* @brief Рассчитывает радиус описанной окружности
-	* @return Радиус описанной окружности
-	*/
 	double RadiusCircle() const;
 };
