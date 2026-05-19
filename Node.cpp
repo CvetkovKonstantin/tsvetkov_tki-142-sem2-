8f0c3616609da9ba5971fc3ca6b6f95a0f451efc
CvetkovKonstantin <praimrprrt@gmail.com>
2026-05-19 11:57:04 +0300

    \Node.cpp

--- Node.cpp
+++ Node.cpp
@@ -1,10 +1,1 @@
-#include "Node.h"
 
-namespace containers {
-
-    template<typename T>
-    Node<T>::Node(const T& value) : data(value), next(nullptr), prev(nullptr) {}
-
-    template class Node<int>;
-
-}
